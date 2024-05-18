fb15b27b604a4d8e89494d14384144bc552adcf2
Naji Jrwan <najijrwanyt@gmail.com>
2024-05-18 21:41:19 +0300

    first commit

---
+++ JS/Fitnesso.js
@@ -0,0 +1,22 @@
+function changeSize1(x)
+{
+    x.style.color = "black";
+    x.style.backgroundColor = "white";
+
+}
+
+function changeSize2(x)
+{
+    x.style.color = "white";
+    x.style.backgroundColor = "black";
+}
+
+function changeSize3(x)
+{
+    x.style.fontSize = "20px";
+}
+
+function changeSize4(x)
+{
+    x.style.fontSize = "16px";
+}
